60fca59f32bd54f2fc7a284b740c42bb3a32b9f2
wbprice <blaine@wbprice.com>
2014-03-04 14:55:16 -0500

    begin to implement routes.

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,23 @@
 Controllers
 ******************/	
 
-var openHealthDataApp = angular.module('openHealthDataApp', [
-	
-]);
+var openHealthDataApp = angular.module('openHealthDataApp', ['ngRoute']);
+
+openHealthDataApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'index.html',
+        controller: 'restaurantListCtrl'
+      }).
+      when('/restaurants/', {
+        templateUrl: 'restaurantview.html',
+        controller: 'restaurantDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
 
 openHealthDataApp.controller('restaurantListCtrl', function ($scope, $http) {
     
@@ -14,3 +28,8 @@ openHealthDataApp.controller('restaurantListCtrl', function ($scope, $http) {
 
 });
 
+openHealthDataApp.controller('restaurantDetailCtrl', function ($scope, $http) {
+    
+
+
+});
--- js/production.js
+++ js/production.js
@@ -2,9 +2,23 @@
 Controllers
 ******************/	
 
-var openHealthDataApp = angular.module('openHealthDataApp', [
-	
-]);
+var openHealthDataApp = angular.module('openHealthDataApp', ['ngRoute']);
+
+openHealthDataApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'index.html',
+        controller: 'restaurantListCtrl'
+      }).
+      when('/restaurants/', {
+        templateUrl: 'restaurantview.html',
+        controller: 'restaurantDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
 
 openHealthDataApp.controller('restaurantListCtrl', function ($scope, $http) {
     
@@ -14,7 +28,11 @@ openHealthDataApp.controller('restaurantListCtrl', function ($scope, $http) {
 
 });
 
+openHealthDataApp.controller('restaurantDetailCtrl', function ($scope, $http) {
+    
 
+
+});
 /******************
 Models
 ******************/
