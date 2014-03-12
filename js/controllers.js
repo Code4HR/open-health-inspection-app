9ef7444b67951d6245924f94bc3a98f5a520264f
wbprice <blaine@wbprice.com>
2014-03-11 21:05:35 -0400

    quick stash for @stanzheng.

--- js/controllers.js
+++ js/controllers.js
@@ -8,19 +8,17 @@ openHealthDataAppControllers.controller('restaurantListCtrl', ['$scope', '$http'
   function($scope, $http) {
     $http.get('restaurants/restaurants.json').success(function(data) {
       $scope.restaurants = data;
+			$scope.map = {
+					center: {
+							latitude: 45,
+							longitude: -73
+					},
+					zoom: 8
+			};
+			console.log($scope);
     });
   }]);
 
-openHealthDataAppControllers.controller('googleMapCtrl', function($scope) {
-    $scope.map = {
-        center: {
-          latitude: 45,
-          longitude: -73
-        },
-        zoom: 8
-    };
-});
-                                        
 openHealthDataAppControllers.controller('restaurantDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
   	$http.get('restaurants/' + $routeParams.id + '.json').success(function(data) {
--- js/production.js
+++ js/production.js
@@ -29,19 +29,17 @@ openHealthDataAppControllers.controller('restaurantListCtrl', ['$scope', '$http'
   function($scope, $http) {
     $http.get('restaurants/restaurants.json').success(function(data) {
       $scope.restaurants = data;
+			$scope.map = {
+					center: {
+							latitude: 45,
+							longitude: -73
+					},
+					zoom: 8
+			};
+			console.log($scope);
     });
   }]);
 
-openHealthDataAppControllers.controller('googleMapCtrl', function($scope) {
-    $scope.map = {
-        center: {
-          latitude: 45,
-          longitude: -73
-        },
-        zoom: 8
-    };
-});
-                                        
 openHealthDataAppControllers.controller('restaurantDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
   	$http.get('restaurants/' + $routeParams.id + '.json').success(function(data) {
@@ -51,6 +49,8 @@ openHealthDataAppControllers.controller('restaurantDetailCtrl', ['$scope', '$rou
 /******************
 Models
 ******************/
+
+console.log(scope);
 /******************
 Views
 ******************/
