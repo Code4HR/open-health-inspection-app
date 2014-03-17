ae6392cce6f46b169af477cc0c5693a0860466e9
wbprice <blaine@wbprice.com>
2014-03-16 21:03:04 -0400

    get google maps working in list view.

--- js/production.js
+++ js/production.js
@@ -29,15 +29,16 @@ openHealthDataAppControllers.controller('restaurantListCtrl', ['$scope', '$http'
   function($scope, $http) {
     $http.get('restaurants/restaurants.json').success(function(data) {
       $scope.restaurants = data;
-			$scope.map = {
-					center: {
-							latitude: 45,
-							longitude: -73
-					},
-					zoom: 8
-			};
-			console.log($scope);
-    });
+		});
+
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        zoom: 8
+    };
+
   }]);
 
 openHealthDataAppControllers.controller('restaurantDetailCtrl', ['$scope', '$routeParams', '$http',
