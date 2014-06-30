f0a528cff6231a62d45f04208e163e74cbbb18fd
stanzheng <dev.stanzheng@gmail.com>
2014-06-29 23:37:13 -0400

    semantic meaning, should be grunt build?

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin']);
+    grunt.registerTask('build', ['concat', 'uglify', 'cssmin', 'imagemin']);
     grunt.registerTask('server', ['connect','open:dev','watch']);
 
 };
