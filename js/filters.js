b03a8b6eb1b7a05fe46cdc233417547e9d9bd209
W. Blaine Price <1wbprice@gmail.com>
2014-07-11 08:51:36 -0400

    begin to build filter to pick right icon.

--- js/filters.js
+++ js/filters.js
@@ -1,5 +1,26 @@
-angular.module('openHealthDataAppFilters', []).filter('was', function() {
-	return function(input) {
-		return input ? 'was' : 'wasn\'t';
-	}
-});
+angular.module('openHealthDataAppFilters', [])
+  .filter('was', function() {
+  	return function(input) {
+  		return input ? 'was' : 'wasn\'t';
+  	}
+  })
+  .filter('category', function(){
+    return function(input) {
+      return "Duh";
+    }
+    // return function(input) {
+    //   switch (expr) {
+    //     case "Seasonal Fast Food Restaurant":
+    //       return "Restaurant";
+    //     case "Fast Food Restaurant":
+    //       return "Restaurant";
+    //     case "Full Service Restaurant":
+    //       return "Restaurant";
+    //     case "Public Middle or High School Food Service":
+    //       return "Education";
+    //     case "Mobile Food Unit":
+    //       return "Mobile Food";
+    //     default:
+    //       return "Other";
+    //   }
+    });
