d3ae8bbcafe13228310ecb90a145b5b03fa737f7
Blaine Price <1wbprice@gmail.com>
2014-08-09 17:12:22 -0400

    Begin to build object containing lat/lons for each city in Virginia.

---
+++ js/cityReference.js
@@ -0,0 +1,268 @@
+{[
+	{
+		name: 'Virginia Beach',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+	},
+  {
+    name: 'Norfolk',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Chesapeake',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Richmond',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Newport News',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Alexandria',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Hampton',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Roanoke',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Portsmouth',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Suffolk',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Lynchburg',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Harrisonburg',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Charlottesville',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Danville',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Manassas',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Petersburg',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Fredericksburg',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Winchester',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Salem',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Staunton',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Fairfax',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Hopewell',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Waynesboro',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Bristol',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Colonial Heights',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Radford',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Manassas Park',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Williamsburg',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Martinsville',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Falls Church',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Poquoson',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Franklin',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Lexington',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Galax',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Buena Vista',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Covington',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Emporia',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  },
+  {
+    name: 'Norton',
+    center: {
+      latitude: ,
+      longitude: 
+    }
+  }
+]}
