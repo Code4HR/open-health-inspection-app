1928e066a1ef97f9c5555d9dda097a83f92ba0ad
Blaine Price <1wbprice@gmail.com>
2014-08-09 17:52:17 -0400

    finished building array containing city names and coordinates.

--- js/cityReference.js
+++ js/cityReference.js
@@ -1,268 +1,268 @@
-{[
+[
 	{
 		name: 'Virginia Beach',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.8506,
+      longitude: -75.9779 
     }
 	},
   {
     name: 'Norfolk',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.9166667,
+      longitude: -76.2000000 
     }
   },
   {
     name: 'Chesapeake',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.7141667,
+      longitude: -76.2383333
     }
   },
   {
     name: 'Richmond',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.5333333,
+      longitude: -77.4666667
     }
   },
   {
     name: 'Newport News',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.0708333,
+      longitude: -76.4844444
     }
   },
   {
     name: 'Alexandria',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.8047222,
+      longitude: -77.0472222
     }
   },
   {
     name: 'Hampton',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.0347222,
+      longitude: -76.3600000
     }
   },
   {
     name: 'Roanoke',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.2708333,
+      longitude: -79.9416667
     }
   },
   {
     name: 'Portsmouth',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.8311111,
+      longitude: -76.3455556
     }
   },
   {
     name: 'Suffolk',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.7411111,
+      longitude: -76.6097222
     }
   },
   {
     name: 'Lynchburg',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.4036111,
+      longitude: -79.1700000
     }
   },
   {
     name: 'Harrisonburg',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.4494444,
+      longitude: -78.8688889
     }
   },
   {
     name: 'Charlottesville',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.0300000,
+      longitude: -78.4788889
     }
   },
   {
     name: 'Danville',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.5872222,
+      longitude: -79.4044444
     }
   },
   {
     name: 'Manassas',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.7513889,
+      longitude: -77.4763889
     }
   },
   {
     name: 'Petersburg',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.2127778,
+      longitude: -77.4002778
     }
   },
   {
     name: 'Fredericksburg',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.3018056,
+      longitude: -77.4708333
     }
   },
   {
     name: 'Winchester',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 39.1833333,
+      longitude: -78.1666667 
     }
   },
   {
     name: 'Salem',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.2866667,
+      longitude: -80.0558333
     }
   },
   {
     name: 'Staunton',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   38.1580556,
+      longitude: -79.0763889
     }
   },
   {
     name: 'Fairfax',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   38.8525000,
+      longitude: -77.3041667
     }
   },
   {
     name: 'Hopewell',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.2902778,
+      longitude:  -77.3050000
     }
   },
   {
     name: 'Waynesboro',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   38.0700000,
+      longitude: -78.8944444
     }
   },
   {
     name: 'Bristol',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   38.0700000,
+      longitude: -78.8944444
     }
   },
   {
     name: 'Colonial Heights',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.2438889,
+      longitude: -77.4105556
     }
   },
   {
     name: 'Radford',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 37.1275000,
+      longitude:  -77.4105556
     }
   },
   {
     name: 'Manassas Park',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   38.7513889,
+      longitude: -77.4763889
     }
   },
   {
     name: 'Williamsburg',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.2708333,
+      longitude:  -76.7069444
     }
   },
   {
     name: 'Martinsville',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.6861111,
+      longitude: -79.8691667
     }
   },
   {
     name: 'Falls Church',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 38.8822222,
+      longitude: -77.1711111
     }
   },
   {
     name: 'Poquoson',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.1330556,
+      longitude: -76.3736111
     }
   },
   {
     name: 'Franklin',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   36.6747222,
+      longitude: -76.9305556
     }
   },
   {
     name: 'Lexington',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.7830556,
+      longitude:  -79.4452778
     }
   },
   {
     name: 'Galax',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   36.6647222,
+      longitude: -80.9202778
     }
   },
   {
     name: 'Buena Vista',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.7322222,
+      longitude: -79.3533333
     }
   },
   {
     name: 'Covington',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   37.7861111,
+      longitude:  -79.9888889
     }
   },
   {
     name: 'Emporia',
     center: {
-      latitude: ,
-      longitude: 
+      latitude: 36.6927778,
+      longitude: -77.5380556
     }
   },
   {
     name: 'Norton',
     center: {
-      latitude: ,
-      longitude: 
+      latitude:   36.9366667,
+      longitude:  -82.6250000
     }
   }
-]}
+]
