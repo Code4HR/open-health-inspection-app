40ba79577e662bf40353014157f1df79a231fd96
Blaine Price <1wbprice@gmail.com>
2014-08-09 18:07:17 -0400

    added population information to each city.

--- js/cityReference.js
+++ js/cityReference.js
@@ -1,6 +1,7 @@
 [
 	{
 		name: 'Virginia Beach',
+    population: 447021,
     center: {
       latitude: 36.8506,
       longitude: -75.9779 
@@ -8,6 +9,7 @@
 	},
   {
     name: 'Norfolk',
+    population: 245782,
     center: {
       latitude: 36.9166667,
       longitude: -76.2000000 
@@ -15,6 +17,7 @@
   },
   {
     name: 'Chesapeake',
+    population: 228417,
     center: {
       latitude: 36.7141667,
       longitude: -76.2383333
@@ -22,6 +25,7 @@
   },
   {
     name: 'Richmond',
+    population: 210309,
     center: {
       latitude: 37.5333333,
       longitude: -77.4666667
@@ -29,6 +33,7 @@
   },
   {
     name: 'Newport News',
+    population: 180726,
     center: {
       latitude: 37.0708333,
       longitude: -76.4844444
@@ -36,6 +41,7 @@
   },
   {
     name: 'Alexandria',
+    population: 146294,
     center: {
       latitude: 38.8047222,
       longitude: -77.0472222
@@ -43,6 +49,7 @@
   },
   {
     name: 'Hampton',
+    population: 136836,
     center: {
       latitude: 37.0347222,
       longitude: -76.3600000
@@ -50,6 +57,7 @@
   },
   {
     name: 'Roanoke',
+    population: 97469,
     center: {
       latitude: 37.2708333,
       longitude: -79.9416667
@@ -57,6 +65,7 @@
   },
   {
     name: 'Portsmouth',
+    population: 96470,
     center: {
       latitude: 36.8311111,
       longitude: -76.3455556
@@ -64,6 +73,7 @@
   },
   {
     name: 'Suffolk',
+    population: 85181,
     center: {
       latitude: 36.7411111,
       longitude: -76.6097222
@@ -71,6 +81,7 @@
   },
   {
     name: 'Lynchburg',
+    population: 77113,
     center: {
       latitude: 37.4036111,
       longitude: -79.1700000
@@ -78,6 +89,7 @@
   },
   {
     name: 'Harrisonburg',
+    population: 50981,
     center: {
       latitude: 38.4494444,
       longitude: -78.8688889
@@ -85,6 +97,7 @@
   },
   {
     name: 'Charlottesville',
+    population: 43956,
     center: {
       latitude: 38.0300000,
       longitude: -78.4788889
@@ -92,6 +105,7 @@
   },
   {
     name: 'Danville',
+    population: 42996,
     center: {
       latitude: 36.5872222,
       longitude: -79.4044444
@@ -99,6 +113,7 @@
   },
   {
     name: 'Manassas',
+    population: 40605,
     center: {
       latitude: 38.7513889,
       longitude: -77.4763889
@@ -106,6 +121,7 @@
   },
   {
     name: 'Petersburg',
+    population: 31973,
     center: {
       latitude: 37.2127778,
       longitude: -77.4002778
@@ -113,6 +129,7 @@
   },
   {
     name: 'Fredericksburg',
+    population: 27307,
     center: {
       latitude: 38.3018056,
       longitude: -77.4708333
@@ -120,6 +137,7 @@
   },
   {
     name: 'Winchester',
+    population: 26881,
     center: {
       latitude: 39.1833333,
       longitude: -78.1666667 
@@ -127,6 +145,7 @@
   },
   {
     name: 'Salem',
+    population: 24970,
     center: {
       latitude: 37.2866667,
       longitude: -80.0558333
@@ -134,6 +153,7 @@
   },
   {
     name: 'Staunton',
+    population: 23921,
     center: {
       latitude:   38.1580556,
       longitude: -79.0763889
@@ -141,6 +161,7 @@
   },
   {
     name: 'Fairfax',
+    population: 23461,
     center: {
       latitude:   38.8525000,
       longitude: -77.3041667
@@ -148,6 +169,7 @@
   },
   {
     name: 'Hopewell',
+    population: 22348,
     center: {
       latitude: 37.2902778,
       longitude:  -77.3050000
@@ -155,6 +177,7 @@
   },
   {
     name: 'Waynesboro',
+    population: 21107,
     center: {
       latitude:   38.0700000,
       longitude: -78.8944444
@@ -162,6 +185,7 @@
   },
   {
     name: 'Bristol',
+    population: 17662,
     center: {
       latitude:   38.0700000,
       longitude: -78.8944444
@@ -169,6 +193,7 @@
   },
   {
     name: 'Colonial Heights',
+    population: 17479,
     center: {
       latitude:   37.2438889,
       longitude: -77.4105556
@@ -176,6 +201,7 @@
   },
   {
     name: 'Radford',
+    population: 16685,
     center: {
       latitude: 37.1275000,
       longitude:  -77.4105556
@@ -183,6 +209,7 @@
   },
   {
     name: 'Manassas Park',
+    population: 15798,
     center: {
       latitude:   38.7513889,
       longitude: -77.4763889
@@ -190,6 +217,7 @@
   },
   {
     name: 'Williamsburg',
+    population: 15167,
     center: {
       latitude:   37.2708333,
       longitude:  -76.7069444
@@ -197,6 +225,7 @@
   },
   {
     name: 'Martinsville',
+    population: 13733,
     center: {
       latitude: 36.6861111,
       longitude: -79.8691667
@@ -204,6 +233,7 @@
   },
   {
     name: 'Falls Church',
+    population: 13229,
     center: {
       latitude: 38.8822222,
       longitude: -77.1711111
@@ -211,6 +241,7 @@
   },
   {
     name: 'Poquoson',
+    population: 12097,
     center: {
       latitude:   37.1330556,
       longitude: -76.3736111
@@ -218,6 +249,7 @@
   },
   {
     name: 'Franklin',
+    population: 8528,
     center: {
       latitude:   36.6747222,
       longitude: -76.9305556
@@ -225,6 +257,7 @@
   },
   {
     name: 'Lexington',
+    population: 6998,
     center: {
       latitude:   37.7830556,
       longitude:  -79.4452778
@@ -232,6 +265,7 @@
   },
   {
     name: 'Galax',
+    population: 6908,
     center: {
       latitude:   36.6647222,
       longitude: -80.9202778
@@ -239,6 +273,7 @@
   },
   {
     name: 'Buena Vista',
+    population: 6707,
     center: {
       latitude:   37.7322222,
       longitude: -79.3533333
@@ -246,6 +281,7 @@
   },
   {
     name: 'Covington',
+    population: 5771,
     center: {
       latitude:   37.7861111,
       longitude:  -79.9888889
@@ -253,6 +289,7 @@
   },
   {
     name: 'Emporia',
+    population: 5740,
     center: {
       latitude: 36.6927778,
       longitude: -77.5380556
@@ -260,6 +297,7 @@
   },
   {
     name: 'Norton',
+    population: 4068,
     center: {
       latitude:   36.9366667,
       longitude:  -82.6250000
